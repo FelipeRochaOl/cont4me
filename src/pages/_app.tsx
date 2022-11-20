db36a001777268ed3f20390b34741fef61a1533a
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-20 15:22:09 -0300

    Add theme in app pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,25 @@
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
+import { ThemeContextProvider } from '../contexts/ThemeContext'
+import { globalStyles } from '../styles/global'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+globalStyles()
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? (page => page)
+  return getLayout(
+    <ThemeContextProvider>
+      <Component {...pageProps} />
+    </ThemeContextProvider>
+  )
+}
+export default App
