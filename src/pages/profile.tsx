f6d904514b47908852ec95e15ad756ba4db52245
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-11-29 02:24:35 -0300

    Create page profile

---
+++ src/pages/profile.tsx
@@ -0,0 +1,159 @@
+import * as Avatar from '@radix-ui/react-avatar'
+import Link from 'next/link'
+import { useState } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import { FaEdit, FaEnvelope, FaPhoneAlt, FaUserAlt } from 'react-icons/fa'
+import { HiOutlineChevronLeft } from 'react-icons/hi'
+import { ImKey } from 'react-icons/im'
+import { ButtonSubmit } from '../components/Form/ButtonSubmit'
+import { Input } from '../components/Form/Input'
+import {
+  Container,
+  ProfileContainer,
+  UserAvatar,
+  UserContainer
+} from '../styles/pages/profile'
+
+type ProfileFormInputs = {
+  name: string
+  email: string
+  phone: string
+  password: string
+  confirmPassword: string
+}
+
+const Profile = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm<ProfileFormInputs>()
+  const [activeField, setActiveField] = useState('name')
+
+  const onSubmit: SubmitHandler<ProfileFormInputs> = ({
+    name,
+    email,
+    phone,
+    password,
+    confirmPassword
+  }) => {
+    console.log({ name, email, phone, password, confirmPassword })
+  }
+  return (
+    <Container>
+      <ProfileContainer>
+        <h1>Profile</h1>
+        <UserContainer>
+          <Link href="" className="return">
+            <HiOutlineChevronLeft size={26} />
+          </Link>
+          <UserAvatar>
+            <Avatar.Root className="avatarContent">
+              <Avatar.Image
+                className="avatarImage"
+                src="https://avatars.githubusercontent.com/u/13703006?v=4&w=283&h=283&dpr=2&q=80"
+                alt="Colm Tuite"
+              />
+              <Avatar.Fallback className="avatarFallback" delayMs={600}>
+                FR
+              </Avatar.Fallback>
+            </Avatar.Root>
+            <FaEdit size={20} />
+          </UserAvatar>
+        </UserContainer>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <Controller
+            name="name"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Input
+                  focused={activeField === 'name'}
+                  icon={<FaUserAlt size={25} />}
+                  type="text"
+                  placeholder="Nome"
+                  onFocus={() => setActiveField('name')}
+                  {...field}
+                  error={errors.name}
+                />
+              )
+            }}
+          />
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Input
+                  focused={activeField === 'email'}
+                  icon={<FaEnvelope size={25} />}
+                  type="email"
+                  placeholder="E-mail"
+                  onFocus={() => setActiveField('email')}
+                  {...field}
+                  error={errors.email}
+                />
+              )
+            }}
+          />
+          <Controller
+            name="phone"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Input
+                  focused={activeField === 'phone'}
+                  icon={<FaPhoneAlt size={25} />}
+                  type="text"
+                  placeholder="Telefone"
+                  onFocus={() => setActiveField('phone')}
+                  {...field}
+                  error={errors.phone}
+                />
+              )
+            }}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Input
+                  focused={activeField === 'password'}
+                  icon={<ImKey size={25} />}
+                  type="password"
+                  placeholder="Senha"
+                  onFocus={() => setActiveField('password')}
+                  {...field}
+                  error={errors.password}
+                />
+              )
+            }}
+          />
+          <Controller
+            name="confirmPassword"
+            control={control}
+            render={({ field }) => {
+              return (
+                <Input
+                  focused={activeField === 'confirmPassword'}
+                  icon={<ImKey size={25} />}
+                  type="password"
+                  placeholder="Confirmar Senha"
+                  onFocus={() => setActiveField('confirmPassword')}
+                  {...field}
+                  error={errors.confirmPassword}
+                />
+              )
+            }}
+          />
+          <ButtonSubmit type="submit" isBig>
+            Salvar
+          </ButtonSubmit>
+        </form>
+      </ProfileContainer>
+    </Container>
+  )
+}
+
+export default Profile
---
+++ src/styles/pages/profile.ts
@@ -0,0 +1,79 @@
+import { styled } from '@stitches/react'
+
+export const Container = styled('main', {
+  paddingTop: '1.25rem',
+  backgroundImage: '$backgroundProfile'
+})
+
+export const ProfileContainer = styled('section', {
+  maxWidth: '620px',
+  margin: '0px auto',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '3.75rem',
+
+  h1: {
+    color: '$green200'
+  },
+
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    padding: '0 57px',
+
+    '.input': {
+      width: '100%',
+      '& + &': {
+        marginTop: '2.188rem'
+      }
+    },
+
+    button: {
+      width: '100%',
+      margin: '2.188rem 0'
+    }
+  }
+})
+
+export const UserContainer = styled('div', {
+  display: 'flex',
+  position: 'relative',
+  width: '100%',
+
+  '.return': {
+    position: 'absolute',
+    bottom: 0,
+    left: 0
+  }
+})
+
+export const UserAvatar = styled('a', {
+  display: 'flex',
+  position: 'relative',
+  margin: '0 auto',
+
+  '.avatarContent': {
+    userSelect: 'none',
+    width: '283px',
+    height: '283px',
+    borderRadius: '100%',
+    outline: '0.1px solid $green200'
+  },
+
+  '.avatarImage': {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    borderRadius: 'inherit'
+  },
+
+  svg: {
+    position: 'absolute',
+    right: '80px',
+    bottom: 0,
+    color: '$green200'
+  }
+})
